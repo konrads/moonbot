16b670875ae0fa937c815525da7c03d82c58e5e9
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2020-09-29 17:47:59 +1000

    ...

--- bin/devops.sh
+++ bin/devops.sh
@@ -190,7 +190,7 @@ case "$trail_arg" in
   "bootstrap-remote")
     echo "# Expected this has already been done:"
     echo "# ...from local:"
-    echo "# ssh -i ~/.ssh/LightsailDefaultKey-ap-southeast-2.pem ubuntu@3.104.151.51"
+    echo "# ssh -i ~/.ssh/LightsailDefaultKey-ap-southeast-2.pem ubuntu@54.252.192.0"
     echo
     echo "...from remote:"
     echo "mkdir ~/src"
