b9b507f3ae342f93f24b0e93ee54d860d7a9067e
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2021-01-03 20:19:34 +1100

    fixing tier calc

--- src/main/scala/moon/TierCalc.scala
+++ src/main/scala/moon/TierCalc.scala
@@ -13,31 +13,33 @@ case class TierCalcImpl(dir: Dir.Value,
                         tierQtyPerc: Double=0.8    /* how much qty decreases per tier*/,
                        ) extends TierCalc {
   assert(dir == LongDir, "*NOT* catering for shorts!")
+  type Price = Double
+  type Quantity = Double
   val firstTradeQty = tradePoolQty / (0 until tierCnt).map(math.pow(tierQtyPerc, _)).sum
-  val qtyTiers = (0 until tierCnt).map(math.pow(firstTradeQty, _))
+  val qtyTiers = (0 until tierCnt).map(n => firstTradeQty * math.pow(tierQtyPerc, n))
 
-  override def canOpenWithQty(currPrice: Double, existingOpenPrices: Seq[Double]): Option[Double /*qty*/] = {
+  override def canOpenWithQty(currPrice: Price, existingOpenPrices: Seq[Price]): Option[Quantity] = {
     if (existingOpenPrices.isEmpty)  // no open orders yet!
       Some(qtyTiers.head)
-    else if (existingOpenPrices.size > tierCnt)  // too many open orders already!
+    else if (existingOpenPrices.size >= tierCnt)  // too many open orders already!
       None
     else {
       val openPrices = existingOpenPrices.sorted
       val highestOpenPrice = openPrices.last
 
-      val tiers = (0 until tierCnt).map { tier =>
+      for(tier <- 0 until tierCnt) {
         val priceHigh = highestOpenPrice * math.pow(tierPricePerc, tier)
         val priceLow = priceHigh * tierPricePerc
-        val qty = qtyTiers(tier)
-        val haveExistingOrder = openPrices.exists(p => priceHigh <= p && p < priceLow)
-        val currPriceInThisTier = priceHigh <= currPrice && currPrice < priceLow
-        (currPriceInThisTier && haveExistingOrder, qty)
+        val currPriceIntThisTier = currPrice > priceLow && currPrice <= priceHigh
+        if (currPriceIntThisTier) {
+          val haveExistingOrder = openPrices.exists(p => p > priceLow && p <= priceHigh)
+          if (haveExistingOrder)
+            return None
+          else
+            return Some(qtyTiers(tier))
+        }
       }
-
-      if (tiers.map(_._1).exists(x => x))
-        None
-      else
-        Some(tiers.head._2)
+      None
     }
   }
 }
---
+++ src/test/scala/moon/TierCalcSpec.scala
@@ -0,0 +1,42 @@
+package moon
+
+import moon.Dir._
+import org.scalatest._
+import org.scalatest.matchers.should._
+
+class TierCalcSpec extends FlatSpec with Matchers with Inside {
+  def createTierCalc = TierCalcImpl(dir=LongDir)
+
+  "TierCalc" should "calculate tiers" in {
+    // tiers: (high, low, amount)
+    //        (10.0,9.5,0.14873869585911467),
+    //        (9.5,9.025,0.11899095668729175),
+    //        (9.025,8.57375,0.09519276534983341),
+    //        (8.573749999999999,8.145062499999998,0.07615421227986673),
+    //        (8.145062499999998,7.737809374999998,0.06092336982389338)
+    createTierCalc.canOpenWithQty(10.0,  Seq()) shouldBe Some(0.14873869585911467)
+    createTierCalc.canOpenWithQty(10.0,  Seq(10.0)) shouldBe None
+    createTierCalc.canOpenWithQty(9.6,   Seq(10.0)) shouldBe None
+
+    createTierCalc.canOpenWithQty(9.5,   Seq(10.0, 9.1)) shouldBe None
+    createTierCalc.canOpenWithQty(9.5,   Seq(10.0)) shouldBe Some(0.11899095668729175)
+    createTierCalc.canOpenWithQty(9.026, Seq(10.0)) shouldBe Some(0.11899095668729175)
+
+    createTierCalc.canOpenWithQty(9.024, Seq(10.0, 9.3, 9.0)) shouldBe None
+    createTierCalc.canOpenWithQty(9.024, Seq(10.0, 9.3)) shouldBe Some(0.09519276534983341)
+    createTierCalc.canOpenWithQty(8.574, Seq(10.0, 9.3)) shouldBe Some(0.09519276534983341)
+
+    createTierCalc.canOpenWithQty(8.4,   Seq(10.0, 9.3, 9.0, 8.3)) shouldBe None
+    createTierCalc.canOpenWithQty(8.573, Seq(10.0, 9.3, 9.0)) shouldBe Some(0.07615421227986673)
+    createTierCalc.canOpenWithQty(8.146, Seq(10.0, 9.3, 9.0)) shouldBe Some(0.07615421227986673)
+
+    createTierCalc.canOpenWithQty(8.0,   Seq(10.0, 9.3, 9.0, 8.3, 7.9)) shouldBe None
+    createTierCalc.canOpenWithQty(8.145, Seq(10.0, 9.3, 9.0, 8.3)) shouldBe Some(0.06092336982389338)
+    createTierCalc.canOpenWithQty(7.738, Seq(10.0, 9.3, 9.0, 8.3)) shouldBe Some(0.06092336982389338)
+
+    createTierCalc.canOpenWithQty(7.3,   Seq(10.0, 9.3, 9.0, 8.3, 7.9)) shouldBe None  // no price below 7.737 results in a trade
+    createTierCalc.canOpenWithQty(6.9,   Seq(10.0)) shouldBe None  // no price below 7.737 results in a trade
+    createTierCalc.canOpenWithQty(11.0,  Seq(10.0, 9.3, 9.0, 8.3, 7.9)) shouldBe None  // no price over 10 results in a trade
+    createTierCalc.canOpenWithQty(12.0,  Seq(10.0)) shouldBe None  // no price over 10 results in a trade
+  }
+}
